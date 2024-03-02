aec5f536bcabfcb5cb79383fec6f6234e1bc8b68
Md Rashed Ali <tools.rashed@gmail.com>
2024-03-03 00:11:03 +0600

    fix /wrong route navigation

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <Link
               className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
-              href="/manage"
+              href="/dashboard"
             >
               Get Started
             </Link>
